9becf6cf7da55fba0272d603d7e4e80216ae9d9a
icefirebear <woungsub1234@gmail.com>
2021-01-07 15:06:57 +0900

    refactor: restructuring db file in class format

--- db.py
+++ db.py
@@ -2,31 +2,28 @@ from typing import List, Any, Optional
 import sqlite3
 
 
-# db 연결
-def init_db(self) -> Any:
-    pass
+class DB:
 
+    # db 연결
+    def init_db(self) -> Any:
+        pass
 
-# 초기 db 테이블 생성
-def create_table() -> Any:
-    pass
+    # 초기 db 테이블 생성
+    def create_table(self) -> Any:
+        pass
 
+    # db 데이터 리스트 반환
+    def list(self, dns: str) -> Any:
+        pass
 
-# db 데이터 리스트 반환
-def read_data() -> Any:
-    pass
+    # db에 dns 추가
+    def add_data_to_file(self, dns: str) -> Any:
+        pass
 
+    # 사용자가 입력한 dns 중 이미 존재하는 dns 있는지 확인
+    def is_exists(self, dns: str) -> bool:
+        pass
 
-# db에 dns 추가
-def add_data_to_file() -> Any:
-    pass
-
-
-# 사용자가 입력한 dns 중 이미 존재하는 dns 있는지 확인
-def is_exists() -> bool:
-    pass
-
-
-# db에 있는 dns 데이터 삭제하기
-def delete_data() -> Any:
-    pass
+    # db에 있는 dns 데이터 삭제하기
+    def delete_data(self, dns: str) -> Any:
+        pass
